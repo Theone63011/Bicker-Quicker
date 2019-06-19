d7692291c675f4004d7d133c7ff5f222c20ac2a9
Benjamin Loisch <ben.loisch@gmail.com>
2019-06-18 21:24:38 -0400

    able to set variables value in the database

--- app/src/main/java/purdue/edu/bicker_quicker/MainActivity.java
+++ app/src/main/java/purdue/edu/bicker_quicker/MainActivity.java
@@ -3,11 +3,19 @@ package purdue.edu.bicker_quicker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.child("message").setValue("gobbleygook");
     }
 }
