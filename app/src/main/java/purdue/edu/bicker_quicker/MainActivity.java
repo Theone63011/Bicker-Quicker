24f7875f4ca43bd20a32b8239cc14a59ac98a4b6
Ed O'Neil <oneil4@purdue.edu>
2019-06-18 21:54:53 -0400

    Added login button

--- app/src/main/java/purdue/edu/bicker_quicker/MainActivity.java
+++ app/src/main/java/purdue/edu/bicker_quicker/MainActivity.java
@@ -1,13 +1,94 @@
 package purdue.edu.bicker_quicker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EMAIL = "email";
+
+    private CallbackManager mCallbackManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCallbackManager = CallbackManager.Factory.create();
+
+        LoginButton mLoginButton = findViewById(R.id.login_button);
+
+        // Set initial permissions to request from the user while logging in
+        mLoginButton.setPermissions(Arrays.asList(EMAIL));
+
+        // Callback registration
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // If successful, the loginResult is the AccessToken
+                // ADD CODE HERE
+
+                View myPopupButtonView = View.getContentView();
+                View.
+                onButtonShowPopupWindowClick(View v);
+
+            }
+
+            @Override
+            public void onCancel() {
+                // ADD CODE HERE
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // ADD CODE HERE
+            }
+        });
     }
+
+    public void onButtonShowPopupWindowClick(View view) {
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.simple_popup_window, null);
+
+        // create the sample popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets touches outside the popup to dismiss the window
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window token
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+
 }
