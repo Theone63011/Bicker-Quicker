68ad586abe5c3b1b6c59bece1eb42e1da708f3b7
thoma558 <thoma558@purdue.edu>
2019-06-26 17:03:35 -0400

    Votes don't show until after voting. Updates user DB with voted bicker ID.

--- app/src/main/java/purdue/edu/bicker_quicker/ViewVoting.java
+++ app/src/main/java/purdue/edu/bicker_quicker/ViewVoting.java
@@ -17,11 +17,16 @@ package purdue.edu.bicker_quicker;
         import android.widget.TextView;
         import android.widget.Toast;
 
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
         import com.google.firebase.database.*;
         import com.google.firebase.database.FirebaseDatabase;
         import com.firebase.ui.auth.AuthUI;
         import com.google.android.gms.tasks.OnCompleteListener;
         import com.google.android.gms.tasks.Task;
+
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class ViewVoting extends AppCompatActivity {
     Button leftVote;
@@ -32,6 +37,8 @@ public class ViewVoting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //have bicker ID passed in
+        //DatabaseReference ref = database.getReference("Bicker/" + bickerID);
         DatabaseReference ref = database.getReference("Bicker/-LhrhW1FAWRsJXfopsuR");
         if (ref == null) {
             System.out.println("Nope");
@@ -42,8 +49,8 @@ public class ViewVoting extends AppCompatActivity {
         rightVote = findViewById(R.id.right);
         noVote = findViewById(R.id.abstain);
         Button exit = findViewById(R.id.exit);
-        final TextView numLeftVotes = findViewById(R.id.leftVotes);
-        final TextView numRightVotes = findViewById(R.id.rightVotes);
+        //final TextView numLeftVotes = findViewById(R.id.leftVotes);
+        //final TextView numRightVotes = findViewById(R.id.rightVotes);
 
         ref.addValueEventListener(new ValueEventListener() {
             @Override
@@ -51,18 +58,18 @@ public class ViewVoting extends AppCompatActivity {
                 final Bicker selectedBicker = dataSnapshot.getValue(Bicker.class);
                 System.out.println("Left votes: " + selectedBicker.getLeft_votes());
                 System.out.println("Right votes: " + selectedBicker.getRight_votes());
-                numLeftVotes.setText(Integer.toString(selectedBicker.getLeft_votes()));
-                numRightVotes.setText(Integer.toString(selectedBicker.getRight_votes()));
+                //numLeftVotes.setText(Integer.toString(selectedBicker.getLeft_votes()));
+                //numRightVotes.setText(Integer.toString(selectedBicker.getRight_votes()));
                 leftVote.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        vote(1, selectedBicker.getLeft_votes() + 1);
+                        vote(1, selectedBicker.getLeft_votes() + 1, selectedBicker.getRight_votes());
                     }
                 });
                 rightVote.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        vote(2, selectedBicker.getRight_votes() + 1);
+                        vote(2,  selectedBicker.getLeft_votes(), selectedBicker.getRight_votes() + 1);
                     }
                 });
             }
@@ -89,17 +96,23 @@ public class ViewVoting extends AppCompatActivity {
     }
 
     //method to update the vote count for left (1) or right (2)
-    public void vote(int response, int updatedCount) {
+    public void vote(int response, int updatedLeftCount, int updatedRightCount) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         //access bicker where id == LhrhW1FAWRsJXfopsuR
         //DatabaseReference ref = response == 1 ? database.getReference("Bicker/-LhrhW1FAWRsJXfopsuR").orderByChild("left_votes") :
         //        database.getReference("Bicker").child("right_votes");
         DatabaseReference ref = database.getReference("Bicker/-LhrhW1FAWRsJXfopsuR");
 
+        //update textfields of voting counts
+        final TextView numLeftVotes = findViewById(R.id.leftVotes);
+        final TextView numRightVotes = findViewById(R.id.rightVotes);
+        numLeftVotes.setText(Integer.toString(updatedLeftCount));
+        numRightVotes.setText(Integer.toString(updatedRightCount));
+
         if (response == 1) {
-            ref.child("left_votes").setValue(updatedCount);
+            ref.child("left_votes").setValue(updatedLeftCount);
         } else if (response == 2) {
-            ref.child("right_votes").setValue(updatedCount);
+            ref.child("right_votes").setValue(updatedRightCount);
         }
         //update User db w/ this bicker's id
         viewVotes();
@@ -107,11 +120,30 @@ public class ViewVoting extends AppCompatActivity {
 
     public void viewVotes() {
         //disable voting buttons, show results
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
         leftVote.setEnabled(false);
         rightVote.setEnabled(false);
         noVote.setEnabled(false);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //List<String> votedBickerIds = new ArrayList<String>();
+        System.out.println("CurrUserID: " + user.getUid());
+        final DatabaseReference ref = database.getReference("User/" + user.getUid());
+        //final User currUser;
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User currUser = dataSnapshot.getValue(User.class);
+                List<String> votedBickerIds = new ArrayList<String>();
+                votedBickerIds = currUser.getVotedBickerIds();
+                votedBickerIds.add("-LhrhW1FAWRsJXfopsuR");
+                ref.child("votedBickerids").setValue(votedBickerIds);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
     }
 
     public void exit() {
