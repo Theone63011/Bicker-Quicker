58cba4bf4735d213865bcc4089d952a2b6301521
Booth4 <Kylebooth4@gmail.com>
2019-06-27 14:45:28 -0400

    Updated profile and sidnout buttonin home drawer. Fixed back buttons leading to bickerPage

--- app/src/main/java/purdue/edu/bicker_quicker/Home_Fragment.java
+++ app/src/main/java/purdue/edu/bicker_quicker/Home_Fragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -90,12 +91,17 @@ public class Home_Fragment extends Fragment {
                 String id = user.getUid();
 
                 for (DataSnapshot userSnapshot : dataSnapshot.child("User").getChildren()){
-                    if(userSnapshot.child("userId").getValue().toString().equals(id)) {
-                        userKey = userSnapshot.getKey();
+                    try {
+                        if (userSnapshot.child("userId") != null && userSnapshot.child("userId").getValue().toString().equals(id)) {
+                            userKey = userSnapshot.getKey();
 
-                        for(DataSnapshot votedId :  userSnapshot.child("votedBickerIds").getChildren()){
-                            votedBickerIds.add(votedId.getValue().toString());
+                            for (DataSnapshot votedId : userSnapshot.child("votedBickerIds").getChildren()) {
+                                votedBickerIds.add(votedId.getValue().toString());
+                            }
                         }
+                    }
+                    catch (Exception e){
+                        Log.w(TAG, "Home_Fragment detected a null user in the database.   " + e);
                     }
                 }
 
