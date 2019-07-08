023e1331dbba4d8a640635297f86d9e18f34650c
thoma558 <thoma558@purdue.edu>
2019-07-08 14:02:32 -0400

    Added button for past bickers and a check for mod status of current user

--- app/src/main/java/purdue/edu/bicker_quicker/ProfileActivity.java
+++ app/src/main/java/purdue/edu/bicker_quicker/ProfileActivity.java
@@ -16,22 +16,34 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
 
     Button respondToBicker;
     Button signOut;
     Button toSettings;
+    Button pastBickers;
     Toolbar toolbar;
+    private static FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme_NoActionBar); // Disable action bar (should be by default but this is precautionary)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        mAuth = FirebaseAuth.getInstance();
         toSettings = findViewById(R.id.settingsButton);
         respondToBicker = findViewById(R.id.bickerRespond);
+        pastBickers = findViewById(R.id.pastBickers);
         toolbar = findViewById(R.id.toolbarBicker);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Your Profile");
@@ -70,6 +82,38 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        pastBickers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pastBickers();
+            }
+        });
+
+        //get user info from DB, check if mod. If so, make mod toggle visible
+        //create listener for PastBickers button in user info retrieval; pass User to pastBickers()
+        FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = database.getReference("User"); //.child("userId").equalTo(currUser.getUid());
+        ref.child("userId").equalTo(currUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                if (user.getModerator()) {
+                    //is mod, set toggle button to true
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void pastBickers() {
+        //pass uID from FirebaseAuth for bicker retrieval where child.equals(uId)
+        Intent intent = new Intent(this, PastBickersActivity.class);
+        startActivity(intent);
     }
 
     public void leave() {
